716912df142b297aa6001d2a33e965e98646f89f
DjedySG <89001015+DjedySG@users.noreply.github.com>
2022-05-06 15:19:35 +0400

    Add files via upload

---
+++ Task_33_5_4.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+
+template <class T>
+T average(T* array, int length)
+{
+    T sum = 0;
+    for (int count = 0; count < length; ++count)
+        sum += array[count];
+
+    sum /= length;
+    return sum;
+}
+
+template < typename T >
+void input(T array[8])
+{
+    std::cout << "Fill the array (8):";
+    for (int i = 0; i < 8; ++i)
+    {
+        std::cin >> array[i];
+    }
+}
+
+int main()
+{
+    int array1[8];
+    input(array1);
+    std::cout << average(array1, 8) << '\n';
+}
+
